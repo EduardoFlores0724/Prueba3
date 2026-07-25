79b27e614c61fc503ffd825321025ffa8dab7799
EduardoFlores0724 <gemereduar456@gmail.com>
2026-07-24 19:55:05 -0600

    Agregamos cosas nuevas

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -15,6 +15,48 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODOO code application logic here
-    }
+        
+       /*
+        
+        */
+        
+        
     
-}
+         System.out.println("Hola ...como estas");
+         System.out.println("Este es mi primer programa");
+         System.out.println("Yujuuu...estoy con Java");
+         
+          System.out.print("Eriz Eduardo");
+          System.out.print("Soy estudiante de programacion");
+          System.out.print("Mucho gusto");
+          
+          System.out.printf("Esto es la clase %S \n","Programacion");
+          //System.out.println("");
+          
+          System.out.printf("Esto es %S la clase de\n","Programacion");
+          //System.out.println("");
+          
+          System.out.printf("%S Esto es la clase de\n","Programacion");
+          //System.out.println("");
+          
+       System.out.printf("Hola \n Como \n estas\n en la clase %s\n?","Programacion");
+       System.out.printf("Hola \t Como \t estas\t en la clase %s\t?","Programacion"); 
+       
+       //Variables
+       int tiempo; //tipo entero 
+       double promedio; //tipo decimal 
+       String nombre; //tipo cadena 
+       char inicial; //tipo caracter -> letra, digitalNumerico; simbolo Especial 
+       boolean estado; //tipo booleano -> true(verdadero /false (falso)
+       
+       tiempo = 3;
+       nombre = "Eriz";
+       inicial = 'E';
+       
+       
+      System.out.printf("Tu nombre es: %s \n",nombre); 
+      System.out.printf("%d veces a la semana recibes la clase\n",tiempo);
+      System.out.printf("Te llamare %c inicial",inicial); 
+    }//Fin de la función MAIN
+    
+}    
